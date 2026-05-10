f975e4399733fb2ba2e8514a72fd124c40b0cce8
Leon Korbee <leon_korbee@hotmail.com>
2026-05-10 21:57:10 +0200

    feat: forward container drag props through ObjectsLayer

--- groei/frontend/src/components/map/ObjectsLayer.tsx
+++ groei/frontend/src/components/map/ObjectsLayer.tsx
@@ -4,38 +4,41 @@ import ObjectShape from './ObjectShape'
 
 interface Props {
   objects: MapObject[]
-  dragPositions: Record<string, { x: number; y: number }>
-  draggingKey: string | null
-  selectedId: string | null
   hoveredContainerId: number | null
   showLabels?: boolean
   heatmapCells?: HeatmapCell[]
   onObjectTap: (object: MapObject) => void
-  onPointerDown: (e: React.PointerEvent, object: MapObject) => void
-  liveRotationId?: string
-  liveRotation?: number
+  onContainerPointerDown?: (e: React.PointerEvent, object: MapObject) => void
+  dragPositions?: Record<string, { x: number; y: number }>
 }
 
-export default function ObjectsLayer({ objects, dragPositions, draggingKey, selectedId, hoveredContainerId, showLabels = true, heatmapCells, onObjectTap, onPointerDown, liveRotationId, liveRotation }: Props) {
+export default function ObjectsLayer({
+  objects,
+  hoveredContainerId,
+  showLabels = true,
+  heatmapCells,
+  onObjectTap,
+  onContainerPointerDown,
+  dragPositions,
+}: Props) {
   return (
     <g>
       {objects.map((obj) => {
-        const key = `object-${obj.id}`
-        const pos = dragPositions[key] ?? { x: obj.map_x ?? 0, y: obj.map_y ?? 0 }
+        const isContainer = obj.category === 'container'
+        const key = `container-${obj.id}`
+        const dragPos = isContainer ? dragPositions?.[key] : undefined
         return (
           <ObjectShape
             key={obj.id}
             object={obj}
-            x={pos.x}
-            y={pos.y}
-            isDragging={draggingKey === key}
-            isSelected={selectedId === key}
+            x={dragPos?.x ?? obj.map_x ?? 0}
+            y={dragPos?.y ?? obj.map_y ?? 0}
             isHoverTarget={hoveredContainerId === obj.id}
             showLabel={showLabels}
             heatmapCells={heatmapCells}
             onTap={onObjectTap}
-            onPointerDown={onPointerDown}
-            liveRotation={liveRotationId === key ? liveRotation : undefined}
+            onPointerDown={isContainer ? onContainerPointerDown : undefined}
+            isDragging={!!dragPos}
           />
         )
       })}
